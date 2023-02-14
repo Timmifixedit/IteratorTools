4bdbde10bd0f30ae9453d9e08e9e5d9a4bd5598f
Timmifixedit <tim@luchterhand.org>
2023-02-14 14:36:27 +0100

    new test structure to enable different c++ versions

--- Test/C++17/Iterators.cpp
+++ Test/C++17/Iterators.cpp
@@ -1,4 +1,5 @@
 #include <gtest/gtest.h>
+#include <ranges>
 #include <vector>
 #include <list>
 #include <string>
---
+++ Test/C++20/Iterators.cpp
@@ -0,0 +1,8 @@
+#include <gtest/gtest.h>
+#include <ranges>
+#include <vector>
+
+
+TEST(cpp20_compat, views) {
+    using namespace std::ranges;
+}
