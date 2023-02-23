59e609b73a69b3f82b8403b3a5861843c554ab76
Timmifixedit <tim@luchterhand.org>
2023-02-23 11:49:22 +0100

    hopefully fixed run script

--- Test/build_and_run_tests_ci.sh
+++ Test/build_and_run_tests_ci.sh
@@ -1,7 +1,7 @@
 echo The current compiler is "$CXX"
 make Tests_C++17 -j"$(nproc)"
 ./Test/C++17/Tests_C++17 --gtest_color=yes
-if [[ $CXX == g++1* ]]; then
+if [[ $CXX == g++-1* ]]; then
   echo running c++20 tests
   make Tests_C++20 -j"$(nproc)"
   ./Test/C++17/Tests_C++20 --gtest_color=yes
