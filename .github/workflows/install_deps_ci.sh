cb40e83f459ce61cd2917d0e8d446928dd55464a
Timmifixedit <tim@luchterhand.org>
2023-02-23 17:02:57 +0100

    back to installation using llvm script

--- .github/workflows/install_deps_ci.sh
+++ .github/workflows/install_deps_ci.sh
@@ -2,10 +2,9 @@ sudo apt update
 if [[ $CXX == g++* ]]; then
   sudo apt install -y "$CXX"
 elif [[ $CXX == clang* ]]; then
-  # sudo apt install -y wget
-  # wget https://apt.llvm.org/llvm.sh
-  # chmod +x llvm.sh
+  sudo apt install -y wget
+  wget https://apt.llvm.org/llvm.sh
+  chmod +x llvm.sh
   CLANG_VERSION=$(echo "$CXX" | tr -d -c 0-9)
-  sudo apt install -y clang-"$CLANG_VERSION"
-  # sudo ./llvm.sh "$CLANG_VERSION"
+  sudo ./llvm.sh "$CLANG_VERSION"
 fi
