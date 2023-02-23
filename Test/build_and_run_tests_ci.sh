18a88ca207f8fff6de951243f09836353d0ca146
Timmifixedit <tim@luchterhand.org>
2023-02-23 15:35:05 +0100

    moved ci scripts to github workflows, separate c++17 and c++20 tests

--- Test/build_and_run_tests_ci.sh
+++
@@ -1,9 +0,0 @@
-echo The current compiler is "$CXX"
-$CXX --version
-make Tests_C++17 -j"$(nproc)"
-./Test/C++17/Tests_C++17 --gtest_color=yes
-if [[ $CXX == g++-1* ]]; then
-  echo running c++20 tests
-  make Tests_C++20 -j"$(nproc)"
-  ./Test/C++20/Tests_C++20 --gtest_color=yes
-fi
