b224af71fd2190a40b756d013f1579c124b1f38e
Timmifixedit <tim@luchterhand.org>
2023-02-23 16:55:13 +0100

    install clang via apt

--- .github/workflows/install_deps_ci.sh
+++ .github/workflows/install_deps_ci.sh
@@ -2,9 +2,10 @@ sudo apt update
 if [[ $CXX == g++* ]]; then
   sudo apt install -y "$CXX"
 elif [[ $CXX == clang* ]]; then
-  sudo apt install -y wget
-  wget https://apt.llvm.org/llvm.sh
-  chmod +x llvm.sh
+  # sudo apt install -y wget
+  # wget https://apt.llvm.org/llvm.sh
+  # chmod +x llvm.sh
   CLANG_VERSION=$(echo "$CXX" | tr -d -c 0-9)
-  sudo ./llvm.sh "$CLANG_VERSION"
+  sudo apt install -y clang-"$CLANG_VERSION"
+  # sudo ./llvm.sh "$CLANG_VERSION"
 fi
